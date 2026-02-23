2482c0cb940b6bf003b99f68e4b9d3d419a287ff
michellerosenstock <michellerosenstock05@gmail.com>
2026-02-23 11:41:58 -0500

    modified files

--- receives_input.py
+++ receives_input.py
@@ -1,13 +1,26 @@
 class receives_input:
- 
+    #handles input. 2 numbers (each >0 and <10). assumes input is valid numbers. greater than 0, less than 10. only 2 numbers.
+    import performs_calc
+
     def __init__(self, a,b):
         self.a = a
         self.b = b
 
-        print(f"Input values:", self.a, self.b)
-  
+        array = [self.a, self.b]
 
-  
+    
+
     def pick_operation(self):
             print("pick an operation: +, -, *, /")
+         
+            operation = input()
+            self.calc=self.performs_calc()
+
+            switcher = {
+                "+": self.calc.add(self.a,self.b),
+                "-": self.calc.subtract(self.a,self.b),
+                "*": self.calc.multiply(self.a,self.b),
+                "/": self.calc.divide(self.a,self.b)
+              
+            }
         
