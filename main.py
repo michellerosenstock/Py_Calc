22ba6eaeb68865540b6cdd742706606916ad72c6
michellerosenstock <michellerosenstock05@gmail.com>
2026-02-23 11:16:04 -0500

    almost there

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 
-
-a = input("Enter the first number: ")
-b = input("Enter the second number: ")
+from receives_input import receives_input
+a = input("Enter the first number: a number betwee 0 and 9 ")
+b = input("Enter the second number: a number between 0 and 9 ")
  
-input_Numbers(a,b)
+input_received = receives_input(a,b)
+
+calc_performed = performs_calc(input_received.a, input_received.b)
 
 
