b42cf03e9d33a22dcd94e36472bff38e1c1ee99a
michellerosenstock <michellerosenstock05@gmail.com>
2026-02-23 10:34:06 -0500

    remove self from multiply func parameter

--- backend.py
+++ backend.py
@@ -31,7 +31,7 @@ class backend:
   def subtract():
     return num1 - num2
   #multiply returns the product of two numbers
-  def multiply(self):
+  def multiply():
     return num1 * num2
   #divide returns the quotient of two numbers
   def divide():
