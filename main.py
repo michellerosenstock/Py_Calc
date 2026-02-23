d7065e42fc9848b7b74c25e3ed58f3857058b9d8
michellerosenstock <michellerosenstock05@gmail.com>
2026-02-23 11:22:19 -0500

    updated main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ a = input("Enter the first number: a number betwee 0 and 9 ")
 b = input("Enter the second number: a number between 0 and 9 ")
  
 input_received = receives_input(a,b)
+input_received.pick_operation()
 
-calc_performed = performs_calc(input_received.a, input_received.b)
 
 
